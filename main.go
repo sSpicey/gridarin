09abfd9bfb5d0f30b8ffb73efae6b355bb3d4fcb
Gabriel Martini <martini@alunos.utfpr.edu.br>
2025-01-22 18:17:25 -0300

    fix font errors & change font

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
-	"github.com/jung-kurt/gofpdf/v2"
+	"path/filepath"
+
+	"github.com/jung-kurt/gofpdf" 
 )
 
-func generatePDFWithCharacters(filename string) {
+// func drawGrid(pdf *gofpdf.Fpdf, x, y, size float64) {
+// }
+
+func main() {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 
-	// Set font
-	pdf.SetFont("Arial", "", 12)
+	fontDir := "/fonts"
+	fontFile := "chinese.msyh.ttf"
 
-	// Define the data
+	pdf.AddUTF8Font("YaHei", "", filepath.Join(fontDir, fontFile))
+
+	x, y := 20.0, 20.0
+	boxSize := 30.0
+
 	data := []struct {
 		English string
 		Pinyin  []string
@@ -23,54 +32,31 @@ func generatePDFWithCharacters(filename string) {
 		{"to welcome", []string{"huān", "yíng"}, []string{"欢", "迎"}},
 	}
 
-	// Set initial position
-	x, y := 10.0, 20.0
-	boxSize := 20.0
-
 	for _, item := range data {
-		// Print English word
+		// English text
+		pdf.SetFont("Arial", "B", 12)
 		pdf.SetXY(x, y)
 		pdf.CellFormat(0, 10, item.English, "", 1, "L", false, 0, "")
 
-		// Print Pinyin and Chinese characters
 		for i, pinyin := range item.Pinyin {
-			// Pinyin
-			pdf.SetXY(x+float64(i)*boxSize, y+10)
+			xPos := x + float64(i)*(boxSize+2)
+
+			// Pinyin text
+			pdf.SetFont("YaHei", "", 12)
+			pdf.SetXY(xPos, y+12)
 			pdf.CellFormat(boxSize, 10, pinyin, "1", 0, "C", false, 0, "")
 
 			// Chinese character
-			pdf.SetXY(x+float64(i)*boxSize, y+20)
-			pdf.SetFont("Arial", "", 24)
+			pdf.SetFont("YaHei", "", 20)
+			pdf.SetXY(xPos, y+22)
 			pdf.CellFormat(boxSize, boxSize, item.Chinese[i], "1", 0, "C", false, 0, "")
-
-			// Draw grid
-			drawGrid(pdf, x+float64(i)*boxSize, y+20, boxSize)
 		}
 
-		// Move to next line
-		y += 2*boxSize + 10
+		y += boxSize + 35
 	}
 
-	// Save the output file
-	err := pdf.OutputFileAndClose(filename)
+	err := pdf.OutputFileAndClose("output.pdf")
 	if err != nil {
 		panic(err)
 	}
-}
-
-func drawGrid(pdf *gofpdf.Fpdf, x, y, size float64) {
-	pdf.SetLineWidth(0.1)
-	pdf.SetDrawColor(0, 255, 0) // Green color for grid
-
-	// Draw diagonal lines
-	pdf.Line(x, y, x+size, y+size)
-	pdf.Line(x+size, y, x, y+size)
-
-	// Draw horizontal and vertical lines
-	pdf.Line(x, y+size/2, x+size, y+size/2)
-	pdf.Line(x+size/2, y, x+size/2, y+size)
-}
-
-func main() {
-	generatePDFWithCharacters("chinese_writing_grid.pdf")
 }
