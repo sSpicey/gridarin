2f94aa5715a7b746f9e51ab29237fad4558ac796
Gabriel Martini <martini@alunos.utfpr.edu.br>
2025-04-04 12:33:00 -0300

    change: reduce box sizes and expand until end of the page

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	data := getLanguageData()
 
 	x, y := 20.0, 20.0
-	boxSize := 30.0
+	boxSize := 20.0
 
 	for _, item := range data {
 		addEnglishText(pdf, item.English, x, y)
@@ -58,19 +58,35 @@ func addEnglishText(pdf *gofpdf.Fpdf, text string, x, y float64) {
 }
 
 func addPinyinAndChinese(pdf *gofpdf.Fpdf, pinyin, chinese []string, x, y, boxSize float64) {
-	for i, p := range pinyin {
+	pageWidth, _ := pdf.GetPageSize()
+	marginLeft, _, marginRight, _ := pdf.GetMargins()
+	usableWidth := pageWidth - marginLeft - marginRight
+	// Calculate how many boxes fit in the usable width
+	numBoxes := int(usableWidth / (boxSize + 2))
+
+	for i := 0; i < numBoxes; i++ {
 		xPos := x + float64(i)*(boxSize+2)
 
-		// Add Pinyin text
-		pdf.SetTextColor(0, 0, 0)
-		pdf.SetFont("YaHei", "", 12)
-		pdf.SetXY(xPos, y+12)
-		pdf.CellFormat(boxSize, 10, p, "1", 0, "C", false, 0, "")
+		// Add Pinyin text if available
+		if i < len(pinyin) {
+			pdf.SetTextColor(0, 0, 0)
+			pdf.SetFont("YaHei", "", 12)
+			pdf.SetXY(xPos, y+12)
+			pdf.CellFormat(boxSize, 10, pinyin[i], "1", 0, "C", false, 0, "")
+		}
 
-		// Add Chinese characters
-		pdf.SetTextColor(200, 200, 200)
-		pdf.SetFont("YaHei", "", 60)
-		pdf.SetXY(xPos, y+22)
-		pdf.CellFormat(boxSize, boxSize, chinese[i], "1", 0, "C", false, 0, "")
+		// Add Chinese characters if available
+		if i < len(chinese) {
+			pdf.SetTextColor(200, 200, 200)
+			pdf.SetFont("YaHei", "", 40)
+			pdf.SetXY(xPos, y+22)
+			pdf.CellFormat(boxSize, boxSize, chinese[i], "1", 0, "C", false, 0, "")
+		} else {
+			// Draw empty grid
+			pdf.SetTextColor(200, 200, 200)
+			pdf.SetFont("YaHei", "", 40)
+			pdf.SetXY(xPos, y+22)
+			pdf.CellFormat(boxSize, boxSize, "", "1", 0, "C", false, 0, "")
+		}
 	}
 }
